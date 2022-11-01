ed1824793ccbf3026d7287286df601d742b5c291
DuudeXXI <buslavicius.vaidas@gmail.com>
2022-11-01 10:18:11 +0200

    issue with client id forfeit

--- clothes_orders/src/Components/admin_orders/List.jsx
+++ clothes_orders/src/Components/admin_orders/List.jsx
@@ -5,14 +5,14 @@ import Line from './Line';
 function List() {
 
     const { list } = useContext(AdminOrdersContext)
-    
+
     return (
             <div className="card" style={{margin: "30px 0 0 0"}}>
             <h5 className="card-header">Selected Type</h5>
             <div className="card-body">
                 <ul className="list-group">
                     {
-                        list?.map(c => <Line key={c[1][0].id} clothe={c} />)
+                         list?.map(c => c[1][0].client_id === null ? null : <Line key={c[1][0].id} clothe={c} />)
                     }
                 </ul>
             </div>
